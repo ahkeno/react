f630de500c3e2ff775f69d07d6d84cc3bab2859c
ahkeno <ahkeno@gmail.com>
2024-07-16 09:05:29 +0630

    authentication react

---
+++ auth/frontend/src/pages/Root.js
@@ -0,0 +1,39 @@
+import { Outlet, useNavigation,useLoaderData,useSubmit } from 'react-router-dom';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
+import MainNavigation from '../components/MainNavigation';
+
+function RootLayout() {
+  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+  // const navigation = useNavigation();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'post' });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' });
+    }, tokenDuration);
+  }, [token, submit]);
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
